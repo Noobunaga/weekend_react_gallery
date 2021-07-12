7d21f28db603c50f26f19a129fbea10cf268df64
Pouapheng Yang <phewy06@yahoo.com>
2021-07-12 09:16:10 -0500

    added a little css

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,10 @@
 const galleryItems = [
-    {id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    {id:2, path:'images/Me_holding_walleye.jpg', description:'Holding the wifes walleye', likes:0},
+    {id:1, path:'images/Baby_pic.jpg', description:'Me in prime colors with the cousins', likes:0},
+    {id:2, path:'images/Getting_helped_out.jpg', description:'My wedding was a bit rough', likes:0},
+    {id:3, path:'images/In_a_tree.jpg', description:'Out on a solo hunt', likes:0},
+    {id:4, path:'images/It_was_hot_in_the_shop.jpg', description:'Trying to blow the cooler air up in the shop', likes:0},
+    {id:5, path:'images/Me_holding_walleye.jpg', description:'Holding the wifes walleye', likes:0},
+
 ];
 
 module.exports = galleryItems;
